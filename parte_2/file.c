03ca74e6d3bff9fee55c71c5cfa203127cf11308
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2022-11-14 16:18:00 -0300

    Add files via upload

---
+++ parte_2/file.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "file.h"
+
+FILE *fileInput1, *fileInput2, *fileOutput;
+
+void open_file(char argv1[], char argv2[]){
+    fileInput1 = fopen(argv1, "r");
+    fileInput2 = fopen(argv2, "r");
+    fileOutput = fopen("saida.txt", "w");
+
+    if(fileInput1 == NULL || fileInput2 == NULL) printf("ERRO AO ABRIR O ARQUIVO.\n");
+}
+
+void read_file_text(char *palavra, char *texto){
+    strcpy(texto, "\0");
+    strcpy(palavra, "\0");
+
+    while (fscanf(fileInput1, "%s", palavra) != EOF){
+        //if(palavra[strlen(palavra)-1] == ',' || palavra[strlen(palavra)-1] == '.') palavra[strlen(palavra)-1] = 0;
+        strcat(texto, palavra);
+        strcat(texto, " ");
+        strcpy(palavra, "\0");
+    }
+
+    strcat(texto, "\0");
+    //printf("%s\n", texto);
+}
+
+int read_file_text2(char *texto){
+    int y;
+    strcpy(texto, "\0");
+    
+    y = fscanf(fileInput1, "%s", texto);
+    //if(texto[strlen(texto)-1] == ',' || texto[strlen(texto)-1] == '.') texto[strlen(texto)-1] = 0;
+    
+    if(y != EOF) return 1;
+    else {
+        rewind(fileInput1);
+        return 0;
+    }
+    //printf("%s\n", texto);
+}
+
+int read_file_pattern(char *padrao){
+    int x;
+    strcpy(padrao, "\0");
+
+    x = fscanf(fileInput2, "%s", padrao);
+
+    if(x != EOF) return 1;
+    else return 0;        
+}
+
+void close_file(){
+    fclose(fileInput1);
+    fclose(fileInput2);
+    fclose(fileOutput);
+}
+
+/*void output(int habilidade){
+    fprintf(fileOutput, "%d ", habilidade);
+}*/
---
+++ parte_2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "file.h"
+#include "bmh.h"
+
+#define MAX_TEXTO 5000
+#define MAX_PALAVRA 50
+#define MAX_PADRAO 50
+
+void main(int argc, char *argv[]){ //./tp3 2 texto.txt padrao.txt
+    open_file(argv[2], argv[3]);
+
+    int escolha = atoi(argv[1]);
+    char *palavra = malloc(MAX_PALAVRA * sizeof(char)); //Maior palavra do texto: 30 caracteres
+    char *texto = malloc(MAX_TEXTO * sizeof(char)); //Texto maximo suportado: 2250 caracteres
+    char *padrao = malloc(MAX_PADRAO * sizeof(char)); //Maior padrao a ser buscado: 30 caracteres
+
+    if (escolha == 1) read_file_text(palavra, texto);
+
+    while(read_file_pattern(padrao)){
+        if(escolha == 1){
+            BMH(texto, strlen(texto), padrao, strlen(padrao));
+        } else if (escolha == 2){
+            
+        }
+    }
+    
+    close_file();
+    free(palavra);
+    free(texto);
+    free(padrao);
+}
