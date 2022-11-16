cd51729474e10635dd70733f4b5668fae3b39f18
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2022-11-16 14:56:22 -0300

    Add files via upload

--- parte_1/file.c
+++ parte_1/file.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "file.h"
 
-FILE *fileInput, *fileOutput;
+FILE *fileInput1, *fileInput2, *fileOutput;
 
 void open_file(char argv1[], char argv2[]){
-    fileInput = fopen(argv1, "r");
-    fileOutput = fopen(argv2, "w");
+    fileInput1 = fopen(argv1, "r");
+    fileInput2 = fopen(argv2, "r");
+    fileOutput = fopen("saida.txt", "w");
 
-    if(fileInput == NULL) printf("ERRO AO ABRIR O ARQUIVO.\n");
+    if(fileInput1 == NULL || fileInput2 == NULL) printf("ERRO AO ABRIR O ARQUIVO.\n");
 }
 
-int read_file(){
-    int x;
-    fscanf(fileInput, "%d", &x);
-    return x;
+void read_file_text(char *palavra, char *texto){
+    strcpy(texto, "\0");
+    strcpy(palavra, "\0");
+
+    while (fscanf(fileInput1, "%s", palavra) != EOF){
+        strcat(texto, palavra);
+        strcat(texto, " ");
+        strcpy(palavra, "\0");
+    }
+
+    strcat(texto, "\0");
+
+    printf("%s\n", texto);
+}
+
+int read_file_pattern(char *padrao, int max){
+    char *x;
+    x = fgets(padrao, max, fileInput2);
+
+    if(x) return 1;
+    else return 0;
+
 }
 
 void close_file(){
-    fclose(fileInput);
+    fclose(fileInput1);
+    fclose(fileInput2);
     fclose(fileOutput);
 }
 
-void output1(int habilidade){
+/*void output(int habilidade){
     fprintf(fileOutput, "%d ", habilidade);
-}
-
-void output2(int vertice, int selecionados){
-    fprintf(fileOutput, "%d %d ", vertice, selecionados);
-}
-
-void output3(){
-    fprintf(fileOutput, "\n");
-}
+}*/
--- parte_1/main.c
+++ parte_1/main.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "file.h"
-#include "estrutura.h"
-#include "heuristica.h"
-#include "dinamica.h"
+
+#define MAX_TEXTO 5000
+#define MAX_PALAVRA 50
+#define MAX_PADRAO 50
 
 void main(int argc, char *argv[]){
-    open_file(argv[2], argv[4]);
+    open_file(argv[2], argv[3]);
 
-    int instancias = read_file(), **matriz, qtdpovos, distancia, peso, qtdcaminhos;
-    Povo *povos, *copia_p;
+    int escolha = atoi(argv[1]);
+    char *palavra = malloc(MAX_PALAVRA * sizeof(char)); //Maior palavra do texto: 30 caracteres
+    char *texto = (char*) malloc(MAX_TEXTO * sizeof(char)); //Texto maximo suportado: 2250 caracteres
+    char *padrao = malloc(MAX_PADRAO * sizeof(char)); //Maior padrao a ser buscado: 30 caracteres
+
+    read_file_text(palavra, texto);
     
-    for (int i = 0; i < instancias; i++){
-        qtdpovos = read_file();
-        distancia = read_file();
-        peso = read_file();
-        qtdcaminhos = read_file();
-
-        matriz = (int**) malloc(qtdpovos * sizeof(int*));
-        povos = malloc(qtdpovos * sizeof(Povo));
-        copia_p = malloc(qtdpovos * sizeof(Povo));
-
-        monta_estrutura(matriz, povos, qtdpovos, qtdcaminhos);
-        copia_povos(copia_p, povos, qtdpovos);
-        dinamica(matriz, povos, qtdpovos, distancia, peso);
-        //heuristica(copia_p, matriz, peso, distancia, qtdpovos);
-        limpa_estrutura(qtdpovos, matriz, povos, copia_p);
+    while (read_file_pattern(padrao, MAX_PADRAO)) {
+        for(int i = 2; i < strlen(padrao); i++) printf("%c", padrao[i-2]);
+        printf("\n");
+        switch (escolha){
+            case 1:
+                break;
+            
+            case 2:
+                break;
+                
+            default:
+                break;
+        }
     }
+    
+    
 
     close_file();
+    free(palavra);
+    free(texto);
+    free(padrao);
 }
