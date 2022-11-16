33b6ad285a1a26c0906cc6d0cd149c48e007775c
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2022-11-16 14:56:40 -0300

    Delete dinamica.h

--- parte_1/dinamica.h
+++
@@ -1,9 +0,0 @@
-struct solucao {
-    int item;
-    struct solucao *next;
-};
-
-typedef struct solucao Solucao;
-
-void dinamica(int **, Povo *, int, int, int);
-int max(Povo *povo, int **tabela, int i, int j);
